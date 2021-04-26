559f9258632aaa1a7f3025d6a68d5a4c04f911d4
Pang Tao <pangtao@mit.edu>
2021-04-26 19:31:23 -0400

    sdf generation and simulation setup.

---
+++ run_sim.py
@@ -0,0 +1,79 @@
+import os
+
+import numpy as np
+
+from pydrake.all import (DiagramBuilder, ConnectMeshcatVisualizer,
+                         Simulator, Parser)
+from pydrake.examples.manipulation_station import ManipulationStation
+from pydrake.math import RigidTransform
+
+
+#%%
+station = ManipulationStation()
+station.SetupClutterClearingStation()
+parser = Parser(station.get_mutable_multibody_plant(),
+                station.get_mutable_scene_graph())
+model_names = ["Cucumber", "Mango", "Lime"]
+
+models_object = {}
+for name in model_names:
+    models_object[name] = []
+    for i in range(3):
+        models_object[name].append(
+            parser.AddModelFromFile(
+                os.path.join("cad_files", name + '.sdf'), name + str(i)))
+
+station.Finalize()
+
+builder = DiagramBuilder()
+builder.AddSystem(station)
+
+ConnectMeshcatVisualizer(
+    builder, scene_graph=station.get_scene_graph(),
+    output_port=station.GetOutputPort("query_object"))
+
+diagram = builder.Build()
+
+#%%
+context = diagram.CreateDefaultContext()
+context_station = diagram.GetSubsystemContext(station, context)
+context_plant = station.GetSubsystemContext(
+    station.get_multibody_plant(), context_station)
+
+#%%
+plant = station.get_multibody_plant()
+model_iiwa = plant.GetModelInstanceByName("iiwa")
+model_gripper = plant.GetModelInstanceByName("gripper")
+model_bin1 = plant.GetModelInstanceByName("bin1")
+model_bin2 = plant.GetModelInstanceByName("bin2")
+
+def get_default_joint_positions(model_idx):
+    q_default = []
+    for joint_idx in plant.GetJointIndices(model_idx):
+        default_positions = plant.get_joint(joint_idx).default_positions()
+        n = len(default_positions)
+        if (n > 0):
+            assert n == 1
+            q_default.append(default_positions[0])
+    return q_default
+
+q_iiwa0 = get_default_joint_positions(model_iiwa)
+
+gripper_position_input_port = station.GetInputPort("wsg_position")
+gripper_position_input_port.FixValue(context_station, [0.05])
+iiwa_position_input_port = station.GetInputPort("iiwa_position")
+iiwa_position_input_port.FixValue(context_station, q_iiwa0)
+
+for name, model_list in models_object.items():
+    X_WObject_list = [
+        RigidTransform(np.array([0.4, 0, 1])),
+        RigidTransform(np.array([0.5, -0.1, 0.7])),
+        RigidTransform(np.array([0.6, -0.2, 0.4]))]
+    for i, model in enumerate(model_list):
+        plant.SetFreeBodyPose(context_plant,
+                              plant.get_body(plant.GetBodyIndices(model)[0]),
+                              X_WObject_list[i])
+
+#%%
+sim = Simulator(diagram, context)
+sim.AdvanceTo(1.0)
---
+++ sdf_generation.py
@@ -0,0 +1,107 @@
+import os
+
+import numpy as np
+from scipy.stats import moment
+from lxml import etree as et
+import trimesh
+
+
+#%% file names
+stl_folder = "cad_files"
+stl_paths = []
+object_names = []
+object_mass = {"Mango": 0.2, "Cucumber": 0.18, "Lime": 0.045}
+model_colors = {
+    "Lime": np.array([191, 255, 0.]) / 255,
+    "Cucumber": np.array([93, 121, 25.]) / 255,
+    "Mango": np.array([246, 189, 22.]) / 255
+}
+
+for file in os.listdir(stl_folder):
+    if file.endswith('.stl'):
+        name, _ = file.rsplit('.', 1)
+        stl_paths.append(os.path.join(stl_folder, file))
+        object_names.append(name)
+
+#%% process mesh
+object_meshes = {}
+for idx in range(len(object_names)):
+    mesh = trimesh.load(stl_paths[idx])
+    scale = 0.001  # mesh vertices are probably in mm.
+    mesh.apply_scale(scale)
+    mesh.apply_transform(mesh.principal_inertia_transform)
+
+    name = object_names[idx]
+    file_name_save = os.path.join(stl_folder, name + "_processed.obj")
+    with open(file_name_save, "w") as file:
+        mesh.export(file, "obj")
+
+    object_meshes[name] = mesh
+
+
+#%%
+name = object_names[0]
+
+root = et.Element("sdf", version="1.7")
+model = et.SubElement(root, "model", name=name)
+link = et.SubElement(model, "link", name="base_link")
+
+# inertial
+inertial = et.SubElement(link, "inertial")
+mass = et.SubElement(inertial, "mass")
+mass.text = str(object_mass[name])
+
+inertia = et.SubElement(inertial, "inertia")
+m = object_mass[name]
+x, y, z = object_meshes[name].bounding_box.extents
+inertia_dict = {
+    "ixx": m / 12 * (y**2 + z**2),
+    "ixy": 0.,
+    "ixz": 0.,
+    "iyy": m / 12 * (x**2 + z**2),
+    "iyz": 0.,
+    "izz": m / 12 * (x**2 + y**2)
+}
+
+for inertia_name, inertia_value in inertia_dict.items():
+    i = et.SubElement(inertia, inertia_name)
+    i.text = '{:.3e}'.format(inertia_value)
+
+# visual
+visual = et.SubElement(link, "visual", name=name + "_visual")
+visual_g = et.SubElement(visual, "geometry")
+visual_g_mesh = et.SubElement(visual_g, "mesh")
+visual_g_mesh_scale = et.SubElement(visual_g_mesh, "scale")
+visual_g_mesh_scale.text = "1 1 1"
+visual_g_mesh_uri = et.SubElement(visual_g_mesh, "uri")
+visual_g_mesh_uri.text = name + "_processed.obj"
+
+# visual RGBA.
+visual_material = et.SubElement(visual, "material")
+visual_material_diffuse = et.SubElement(visual_material, "diffuse")
+visual_material_diffuse.text = ''
+for c in model_colors[name]:
+    visual_material_diffuse.text += "{:.2} ".format(c)
+visual_material_diffuse.text += '1.0'
+
+# collision
+collision = et.SubElement(link, "collision", name=name + "_collision")
+collision_pose = et.SubElement(collision, "pose")
+xc, yc, zc = object_meshes[name].bounding_box.center_mass
+collision_pose.text = ('{:.3e}'.format(xc) + ' '
+                       + '{:.3e}'.format(yc) + ' '
+                       + '{:.3e}'.format(zc) + ' 0 0 0')
+collision_g = et.SubElement(collision, "geometry")
+collision_g_box = et.SubElement(collision_g, "box")
+collision_g_box_size = et.SubElement(collision_g_box, "size")
+collision_g_box_size.text = ('{:.3e}'.format(x) + ' ' +
+                             '{:.3e}'.format(y) + ' ' +
+                             '{:.3e}'.format(z))
+
+
+sdf_binary = et.tostring(root, pretty_print=True)
+# print(sdf_binary.decode('utf-8'))
+
+file_name_save = os.path.join(stl_folder, name + ".sdf")
+with open(file_name_save, "w") as file:
+    file.write(sdf_binary.decode('utf-8'))
